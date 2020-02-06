607c64ca6aaeccebc55a337038a4006fdfc4c355
tomas <tomas.river12@gmail.com>
2020-02-05 23:02:11 -0300

    ejercicios realizados

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,22 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+  var importe;
+  var op;
+  var resultado;
+  var op2;
+
+  importe=document.getElementById("importe").value;
+
+  importe=parseInt(importe);
+  op=(importe * 25);
+  op2=(op / 100);
+
+  resultado=(importe - op2);
+
+  document.getElementById("resultado").value=resultado;
+  
+
+
+
 }
