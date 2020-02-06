aad66238709ceadcf880135d4863b6ab68f69194
tomas <tomas.river12@gmail.com>
2020-02-06 15:10:39 -0300

    ejercicios realizados

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -3,7 +3,9 @@ function mostrar()
 //tomo la edad  
 var edad;
 edad=document.getElementById("edad").value;
-if(edad >=18) {
+if(edad >=18) {  alert ("Es Mayor de edad") }
+
+if (edad <18) {alert("Es menor de edad") }
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+edad=document.getElementById("edad").value;
+
+if(edad >12) if(edad )
+ {alert("Usted es adolescente") }
 
 
 
