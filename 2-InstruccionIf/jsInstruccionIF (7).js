4cb117dfe32d14072a0e530875b8b7700c6d321c
tomas <tomas.river12@gmail.com>
2020-02-11 19:37:29 -0300

    Ejercicios hechos

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,10 @@
 function mostrar()
 {
 //tomo la edad  
-
-	
+edad=document.getElementById("edad").value;
+estadocivil=document.getElementById("estadoCivil").value;
+if(edad < 18 && estadocivil !=="Soltero")
+{alert("Es muy pequeño para NO ser soltero")}
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,8 +1,10 @@
 function mostrar()
 {
 //tomo la edad  
-
-	
+edad=document.getElementById("edad").value;
+estado=document.getElementById("estadoCivil").value;
+if(edad  >=18 && estado =="Soltero")
+{alert("Es soltero y no es menor")}
 
 
 }//FIN DE LA FUNCIÓN
